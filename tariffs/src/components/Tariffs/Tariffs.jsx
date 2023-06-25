c5f4502bfaabc1d9771c9f3906a19e4748746845
Nellyktv <kitkaty965@gmail.com>
2023-06-25 18:48:18 +0300

    added files

---
+++ tariffs/src/components/Tariffs/Tariffs.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Tariffs.scss";
+
+
+class Tariffs extends React.Component{
+render(){
+const {name,theme_nameTariff ,theme_tariff, price, speed, isSelected}= this.props;
+let classCard='';
+if(isSelected) classCard="selected";
+return(
+    
+<div className={`container-tariff  ${classCard}`}>
+    <p className={theme_nameTariff}>Безлимитный {name}</p>
+
+    <div className={theme_tariff} >
+        <div className="tariff_rub">руб.</div>
+        <div className="tariff_price">{price}</div>
+        <div className="tariff_month">/мес</div>
+    </div>
+
+    <div className='speedStyle blocks_nameTariff'>до {speed} Мбит/сек </div>
+    <div className="volumeTraffic">
+        <p>Объем включенного трафика не ограничен</p>
+    </div>
+</div>
+);
+}
+}
+
+export default Tariffs ;
